21e4dc08089ce9fb423e6fd7e5e044d5105a9120
JoeBenassi <Joebenassi@gmail.com>
2013-08-07 01:04:00 -0400

    Add Javadocs to applications/App.class

--- JCAT/src/applications/App.java
+++ JCAT/src/applications/App.java
@@ -20,16 +20,10 @@ import packets.tlm.TlmPkt;
 import utilities.TelemetryUpdater;
 
 /**
- * IN DEVELOPMENT. SUBJECT TO CHANGE.
- * 
- * A dynamic and versatile class that can represent any Command and Telemetry
- * App. Every App is an instance of this class with App-specific parameters. The
- * parameters are configured from an XML document through the
- * packets.housekeeping.XMLParser class. This is done through File -> Populate
- * Apps and selecting appropriately-formatted XML files on your computer.
+ * FULLYDOCUMENTED!
+ * A class that has an instance for each App Profile loaded.
  * 
  * @author Joe Benassi
- * 
  */
 public final class App {
 	private final ArrayList<CmdPkt> commands;
@@ -44,36 +38,37 @@ public final class App {
 	 * 
 	 * test
 	 * 
+	 * 
+	 * 
+	 */
+	/**
 	 * This is a Command and Telemetry app configured with app-specific
 	 * parameters, and can represent any Command and Telemetry app. Every app is
 	 * an instance of this class with app-specific parameters. The parameters
-	 * are configured from an XML document through the
-	 * packets.housekeeping.XMLParser class. This is done through File ->
-	 * Populate Apps and selecting appropriately-formatted XML files on your
-	 * computer.
-	 * @param config 
+	 * are configured from an XML document through the utilities.XMLParser
+	 * class. This is done through File > Populate Apps and selecting
+	 * appropriately-formatted XML files on your computer.
 	 * 
-	 * @param displayName
-	 *            The name that appears in the menu. Specific to an App.
-	 * @param popupName
-	 *            The name that appears in the popup telemetry page. Specific to
-	 *            an App.
-	 * @param prefix
-	 *            The prefix name specific to an App.
+	 * @param name
+	 *            The name to display for this App in the GUI.
+	 * @param config
+	 *            The name of the Config, to be displayed in the GUI.
 	 * @param commands
-	 *            The command information specific to an App. This input
-	 *            ArrayList should contain every CmdPkt for this App, with each
-	 *            fully defined in all ways, including its parameters.
+	 *            The commands for this App.
 	 * @param telemetry
-	 *            The telemetry information specific to an App. This input array
-	 *            should contain all of the telemetry qualities possible to
-	 *            display for the App. For example, if this were the Executive
-	 *            Services app, telemetry.length = 35.
-	 * @param appID
-	 *            The ID specific to an App.
+	 *            The telemetry for this App.
+	 * @param TlmAppID
+	 *            The telemetry msgid. If an incoming packet has a msgid
+	 *            matching this, the packet will be treated as a houskeeping
+	 *            packet to update this App's telemetry values in the GUI.
+	 * @param CmdAppID
+	 *            The command msgid. If the CFS has an App whose command msgid
+	 *            matches this, the CFS will treat this as a command packet for
+	 *            the App.
 	 */
-	public App(final String name, String config, final ArrayList<CmdPkt> commands,
-			final TlmPkt[] telemetry, final int TlmAppID, final int CmdAppID) {
+	public App(final String name, String config,
+			final ArrayList<CmdPkt> commands, final TlmPkt[] telemetry,
+			final int TlmAppID, final int CmdAppID) {
 		final String[] entryNames = new String[telemetry.length];
 
 		for (int i = 0; i < entryNames.length; i++) {
@@ -88,21 +83,29 @@ public final class App {
 		this.telemetry = telemetry;
 
 		popupFiller = new PopupFiller(getMenuName(), entryNames);
-		
+
 		Networker.addApp(this);
 		Launcher.addUserActivity("IMPORTED APP PROFILE: " + getMenuName());
 	}
 
 	/**
-	 * Returns the ID of this particular App. Used to differentiate Apps
-	 * behind-the-scenes.
+	 * Returns the tlmmid of this particular App. Used to determine if a
+	 * telemetry housekeeping packet belongs to this App.
 	 * 
-	 * @return the ID of this particular App.
+	 * @return The tlmmid of this particular App.
 	 */
 	public final int getTlmAppID() {
 		return TlmAppID;
 	}
-	
+
+	/**
+	 * Sets the "SC" or "Sequence Count" value in this App's telemetry window.
+	 * This is used when a telemetry packet is parsed for its SC value, and
+	 * needs to be displayed in the GUI.
+	 * 
+	 * @param SC
+	 *            The string to represent as this App's current Sequence Count.
+	 */
 	public final void setSC(final String SC) {
 		Display.getDefault().syncExec(new Runnable() {
 			@Override
@@ -111,7 +114,16 @@ public final class App {
 			}
 		});
 	}
-	
+
+	/**
+	 * Sets the Time value in this App's telemetry window. This is used when a
+	 * telemetry packet is parsed for its Time value, and needs to be displayed
+	 * in the GUI.
+	 * 
+	 * @param time
+	 *            The string to represent as this App's current Time, as
+	 *            determined by the CFS App.
+	 */
 	public final void setTime(final String time) {
 		Display.getDefault().syncExec(new Runnable() {
 			@Override
@@ -121,43 +133,91 @@ public final class App {
 		});
 	}
 
+	/**
+	 * Returns a string representing the value of a telemetry parameter. The
+	 * input "index" determines the particular telemetry parameter.
+	 * 
+	 * @param index
+	 *            The index of the telemetry parameter within this App's
+	 *            telemetry housekeeping packet.
+	 * @return The string representing the value of the telemetry parameter in
+	 *         question.
+	 */
 	public final String getTelemetryValue(int index) {
 		return telemetry[index].getValue();
 	}
 
+	/**
+	 * Returns the GUI object that displays the telemetry parameter's value. The
+	 * input "index" determines the particular telemetry parameter.
+	 * 
+	 * @param index
+	 *            The index of the telemetry parameter within this App's
+	 *            housekeeping packet.
+	 * @return The Text object that contains the string representing the value
+	 *         of the telemetry parameter in question.
+	 */
 	public final Text getTelemetryText(int index) {
 		return popupFiller.getText(index);
 	}
 
+	/**
+	 * Returns the amount of telemetry parameters in this App's housekeeping
+	 * packet.
+	 * 
+	 * @return The integer whose value equals the amount of telemetry parameters
+	 *         in this App's housekeeping packet.
+	 */
 	public final int getTelemetryAmt() {
 		return telemetry.length;
 	}
 
+	/**
+	 * Executes the command at the input index with the input parameters.
+	 * 
+	 * @param index
+	 *            The index of the desired command within this App's commands.
+	 * @param parameters
+	 *            The string array containing the parameters to pack in this
+	 *            command.
+	 */
 	public final void executeCommand(int index, String[] parameters) {
 		commands.get(index).execute(parameters);
 	}
 
 	/**
-	 * Returns the name of this particular App.
+	 * Returns the name of this App as it would appear in the menu. This equals:
+	 * name [config].
 	 * 
-	 * @return The name of this particular App.
+	 * @return A string that represents this App in the menu.
 	 */
 	public final String getMenuName() {
 		return name + " [" + config + "] ";
 	}
-	
+
+	/**
+	 * Returns the name of this App.
+	 * 
+	 * @return A string representing the name of this App.
+	 */
 	public final String getName() {
 		return name;
 	}
-	
+
+	/**
+	 * Returns the name of this App's config.
+	 * 
+	 * @return A string representing the name of this App's config.
+	 */
 	public final String getConfig() {
 		return config;
 	}
 
 	/**
-	 * Returns the names of the commands, as displayed in the Menu.
+	 * Returns an array of strings, where each string is the name of a command
+	 * for this App.
 	 * 
-	 * @return the names of the commands, as displayed in the Menu.
+	 * @return The names of the commands for this App.
 	 */
 	public final String[] getCommandNames() {
 		String[] names = new String[commands.size()];
@@ -172,7 +232,7 @@ public final class App {
 	 * parameter-input window for that Command.
 	 * 
 	 * @param index
-	 *            the index of the desired Command within this App.
+	 *            The index of the desired Command within this App.
 	 * @return A Listener for a Command that, when executed, launches the
 	 *         parameter-input window for that Command.
 	 */
@@ -196,7 +256,8 @@ public final class App {
 	 * 
 	 * @param shell
 	 *            The shell containing the main Shell of the app.
-	 * @return A Listener that launches the popup telemetry window when executed
+	 * @return A Listener that launches the popup telemetry window when
+	 *         executed.
 	 */
 	public final Listener getTelemetryListener(final Shell shell) {
 		return new Listener() {
@@ -208,14 +269,31 @@ public final class App {
 		};
 	}
 
+	/**
+	 * Returns the array of TlmPkts for this App.
+	 * 
+	 * @return The telemetry objects for this App.
+	 */
 	public final TlmPkt[] getTelemetry() {
 		return telemetry;
 	}
 
-	public final void ingest(CcsdsTlmPkt TlmPkt) {
-		TelemetryUpdater.updateTelemetry(TlmPkt, this);
+	/**
+	 * Using the input CcsdsTlmPkt, this App updates its TlmPkts and their
+	 * representation in the GUI.
+	 * 
+	 * @param ccsdsTlmPkt
+	 *            The CcsdsTlmPkt to parse for telemetry information and update
+	 *            the App accordingly.
+	 */
+	public final void ingest(CcsdsTlmPkt ccsdsTlmPkt) {
+		TelemetryUpdater.updateTelemetry(ccsdsTlmPkt, this);
 	}
 
+	/**
+	 * Returns the command ID for this App.
+	 * @return the cmdmid for this App.
+	 */
 	public int getCmdId() {
 		return CmdAppID;
 	}
